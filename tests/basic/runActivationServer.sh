f9bf1fc45fdef4a60e6dd1b79da09524375559aa
Brian Freeman <bfreeman@optonline.net>
2020-10-16 16:49:35 +0000

    Fixes for basic and robot
    remove status service
    update to AF_INET socket for ipaddresses
    
    Signed-off-by: Brian Freeman <bfreeman@optonline.net>

--- tests/basic/runActivationServer.sh
+++ tests/basic/runActivationServer.sh
@@ -13,4 +13,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #============LICENSE_END===============================================================================================================
-python activation_server.py
+python3 activation_server.py
